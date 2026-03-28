689b3b3c13f14ce141d2b4f01e82503c776b6360
ValentijnFust <valentijn.fust@gmail.com>
2026-03-28 12:17:36 +0100

    Added the files and code required to add custom blocks to minecraft

--- src/main/java/net/mod/permafrostpeaks/block/ModBlocks.java
+++ src/main/java/net/mod/permafrostpeaks/block/ModBlocks.java
@@ -1,16 +1,27 @@
 package net.mod.permafrostpeaks.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.mod.permafrostpeaks.PermaFrostPeaks;
 
 public class ModBlocks {
 
-    private static Block RegisterBlock(String name, Block block) {
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK )));
+    public static final Block ANTONIO_LIBERTO_BLOCK = registerBlock("antonio_liberto_block",
+            new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.HONEY )));
+
+
+
+    private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, Identifier.of(PermaFrostPeaks.MOD_ID, name), block);
     }
@@ -20,8 +31,14 @@ public class ModBlocks {
                 new BlockItem(block, new Item.Settings()));
     }
 
-
     public static void registerModBlocks() {
         PermaFrostPeaks.LOGGER.info("Registering Mod Blocks for " + PermaFrostPeaks.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK);
+                });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.ANTONIO_LIBERTO_BLOCK);
+        });
     }
 }
