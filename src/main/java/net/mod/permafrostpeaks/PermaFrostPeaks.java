6ea803db4489b8f5184174965682377dde60a45c
ValentijnFust <valentijn.fust@gmail.com>
2026-06-29 15:33:33 +0200

    Added a function which moves the portal upwards to prevent it from being placed in caves

--- src/main/java/net/mod/permafrostpeaks/PermaFrostPeaks.java
+++ src/main/java/net/mod/permafrostpeaks/PermaFrostPeaks.java
@@ -1,10 +1,15 @@
 package net.mod.permafrostpeaks;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.*;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.util.Identifier;
@@ -14,23 +19,18 @@ import net.mod.permafrostpeaks.entity.ModEntities;
 import net.mod.permafrostpeaks.entity.custom.TrollEntity;
 import net.mod.permafrostpeaks.item.ModItemGroups;
 import net.mod.permafrostpeaks.item.ModItems;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.passive.SheepEntity;
-import net.minecraft.item.Items;
-import net.minecraft.potion.Potions;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import net.mod.permafrostpeaks.screen.ModScreenHandlers;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PermaFrostPeaks implements ModInitializer {
 	public static final String MOD_ID = "permafrostpeaks";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final RegistryKey<World> PERMAFROST_DIMENSION_KEY = RegistryKey.of(
+            RegistryKeys.WORLD,
+            Identifier.of(MOD_ID, "permafrostpeaks")
+    );
 
 	@Override
 	public void onInitialize() {
@@ -60,6 +60,7 @@ public class PermaFrostPeaks implements ModInitializer {
         ModScreenHandlers.registerScreenHandlers();
 
         //Dimension
+
         CustomPortalBuilder.beginPortal()
                 .frameBlock(ModBlocks.GREEN_CRYSTAL_BLOCK)
                 .lightWithItem(ModItems.GREEN_CRYSTAL_SHARD)
@@ -88,5 +89,57 @@ public class PermaFrostPeaks implements ModInitializer {
                 .tintColor(232, 209, 0)
                 .registerPortal();
 
+        ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> {
+            if (!destination.getRegistryKey().equals(PERMAFROST_DIMENSION_KEY)) {
+                return;
+            }
+
+            movePlayerToPermafrostSurface(player, destination);
+        });
+    }
+
+    private static void movePlayerToPermafrostSurface(ServerPlayerEntity player, ServerWorld world) {
+        int x = player.getBlockX();
+        int z = player.getBlockZ();
+
+        // Make sure the chunk is loaded before asking the heightmap.
+        world.getChunk(x >> 4, z >> 4);
+
+        int surfaceY = world.getTopY(
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                x,
+                z
+        );
+
+        BlockPos safePos = findSafeSurfacePosition(world, x, surfaceY, z);
+
+        player.teleport(
+                world,
+                safePos.getX() + 0.5,
+                safePos.getY(),
+                safePos.getZ() + 0.5,
+                player.getYaw(),
+                player.getPitch()
+        );
+    }
+
+    private static BlockPos findSafeSurfacePosition(ServerWorld world, int x, int startY, int z) {
+        // First try around the heightmap result.
+        for (int y = startY + 4; y >= world.getBottomY(); y--) {
+            BlockPos feet = new BlockPos(x, y, z);
+            BlockPos head = feet.up();
+            BlockPos ground = feet.down();
+
+            if (
+                    world.getBlockState(ground).isSolidBlock(world, ground)
+                            && world.getBlockState(feet).isAir()
+                            && world.getBlockState(head).isAir()
+            ) {
+                return feet;
+            }
+        }
+
+        // Fallback: if somehow no safe surface was found, place the player high.
+        return new BlockPos(x, startY + 2, z);
     }
 }
